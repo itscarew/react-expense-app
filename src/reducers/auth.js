ab53be796da01f6d652190ec39a8cc7492c2ddc1
itscarew <itscarew@gmail.com>
2019-11-11 14:03:09 +0100

    first commit

--- src/reducers/auth.js
+++
@@ -1,9 +0,0 @@
-export default (state = {}, action) => {
-  if (action.type === "LOGIN") {
-    return {
-      uid: action.uid
-    };
-  } else if (action.type === "LOGOUT") {
-    return {};
-  } else return state;
-};
--- src/selector/expense-total.js
+++
@@ -1,5 +0,0 @@
-export default expense => {
-  return expense
-    .map(expense => expense.amount)
-    .reduce((sum, val) => sum + val, 0);
-};
